6ffb568263c36539d424b5c62f98888284c11e14
xyzsuf <yusuf_in@outlook.com>
2022-11-24 19:24:40 +0530

    fixed bug

--- calculator/calc.py
+++ calculator/calc.py
@@ -29,17 +29,17 @@ def calculate():
         should_continue = True 
 
     while should_continue:
-        operation_symbol = input("Pick your operation:" )
+        symbol = input("Pick your operation:" )
         x2  = float( input("What's the next number: "))
-        calc = operators[operation_symbol]
+        calc = operators[symbol]
         answer = calc (x1,x2)
         
 
-        print(f"{x1} {operation_symbol} {x2} = {answer}")
+        print(f"{x1} {symbol} {x2} = {answer}")
         if input(f"Type 'y' to continue calculating with {answer} ") == 'y':
             x1 = answer
         else:
             should_continue = False
-            calculator()
+            calculate()
 
 calculate()
