4219b3aa09072f0066fba3082c53c2f1717ab262
xyzsuf <yusuf_in@outlook.com>
2023-01-03 23:11:27 +0530

    tkinter layouts

---
+++ tkinter-layouts.py
@@ -0,0 +1,43 @@
+from tkinter import *
+
+# Pack -  used to pack widgets in order
+# Place - used for precise positioning(very precise so difficult to use)
+# Grid - uses rows x columns 
+
+
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+# adding padding 
+window.config(padx=100, pady=200)
+
+#Label
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.config(text="New Text")
+#my_label.pack(side="left")
+#my_label.place(x=0,y=0)
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
+
+#Button
+button = Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+#Entry
+input = Entry(width=10)
+print(input.get())
+input.grid(column=3, row=2)
+
+
+
+window.mainloop()
