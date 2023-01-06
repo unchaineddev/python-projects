2ef2ffe4f1ba321cc01bd1520827d09b887b8c88
xyzsuf <yusuf_in@outlook.com>
2023-01-06 22:35:30 +0530

    timer

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
+import math
 
-# ---------------------------- CONSTANTS ------------------------------- #
+# CONSTANTS
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -12,16 +13,40 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 
-# UI 
+# Timer Mechanism
 
+def start_timer():
+    # CountDown 
+    count_down(5*60)
+
+
+# Countdown 
+
+# Creating a timer for countdown using "window.after"
+###window.after(count_in_ms, function,*args )###
+
+def count_down(count):
+    count_minute = math.floor(count/60)
+    count_second = count % 60
+    if count_second <10:
+        count_second = f"{count_second}0"
+
+    canvas.itemconfig(timer_text, text=f"{count_minute}:{count_second}")
+    print(count)
+    if count>0:
+        window.after(1000,count_down,count-1)
+
+
+
+# User Interface 
+
+# Creating a window
 window = Tk();
 window.title("Libredoro")
 window.config(padx=100,pady=50,bg=YELLOW)
+
 
 # Labels
 timer = Label(text="Timer",fg=GREEN,font=(FONT_NAME,40),bg=YELLOW)
@@ -34,14 +59,18 @@ tomato_img = PhotoImage(file="tomato.png")
 #Creating image
 canvas.create_image(100, 112, image=tomato_img)
 #Creating text
-canvas.create_text(100,129, text="00:00",fill="white",font=(FONT_NAME,32,"bold"))
+timer_text = canvas.create_text(100,129, text="00:00",fill="white",font=(FONT_NAME,32,"bold"))
 canvas.grid(column=2, row=2)
+
+
+
 
 
 # Creating Buttons
 
-start = Button(text="Start", highlightthickness=0)
+start = Button(text="Start", highlightthickness=0,command=start_timer)
 start.grid(column=1,row=3)
+
 stop = Button(text="Stop", highlightthickness=0)
 stop.grid(column=3, row=3)
 
