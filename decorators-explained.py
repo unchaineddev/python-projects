e04df316c5620b7c97415ff839e3c8a3946dd884
xyzsuf <yusuf_in@outlook.com>
2023-03-23 17:17:58 +0530

    @

--- decorators-explained.py
+++ decorators-explained.py
@@ -1,22 +1,48 @@
-def add(n1,n2):
-    return n1 + n2 
-
-def sub(n1,n2):
-    return n1 - n2
-
-def mul(n1,n2):
-    return n1 * n2
-
-def div(n1, n2):
-    return n1 /n2
-
+#def add(n1,n2):
+#    return n1 + n2 
+#
+#def sub(n1,n2):
+#    return n1 - n2
+#
+#def mul(n1,n2):
+#    return n1 * n2
+#
+#def div(n1, n2):
+#    return n1 /n2
+#
+#
+## Decorator 
+#""" First class Objects - can be passed as aruguments"""
+#
+#def calculate(calculate_function, n1, n2):
+#    return calculate_function(n1,n2)
+#
+#result = calculate(add, 2, 3)
+#print(result)
+#
 
 # Decorator 
-""" First class Objects - can be passed as aruguments"""
+#def decorator_func(function):
+#    def wrapper_func():
+#        function()
+#    return wrapper_func
+#
 
-def calculate(calculate_function, n1, n2):
-    return calculate_function(n1,n2)
 
-result = calculate(add, 2, 3)
-print(result)
+import time 
+
+def delay_decorator(function):
+    def wrapper_function():
+        time.sleep(2)
+        function()
+
+    return wrapper_function
+
+@delay_decorator
+def hello():
+    print("Hello")
+
+@delay_decorator
+def bye():
+    print("bye")
 
