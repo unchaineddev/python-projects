2881f6876817e0fae8e1451e0785c780af0e4e32
yusuf <yusuftechie+git@gmail.com>
2024-01-05 11:35:00 +0530

    adding decoding weg page 2

--- practicepython.org/19.decode-a-web-page-two.py
+++ practicepython.org/19.decode-a-web-page-two.py
@@ -9,19 +9,15 @@ read the full article without having to click any buttons.
 import requests
 from bs4 import BeautifulSoup
 
-
 res = requests.get("https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture")
 content = res.text
 
 soup = BeautifulSoup(content, "html.parser")
-
-
 title = soup.find(class_= "BaseWrap-sc-gjQpdd BaseText-ewhhUZ ContentHeaderHed-NCyCC iUEiRd euHXZX gydgrM")
 
 print(title.text)
 
 data = soup.find_all("p", class_="paywall")
-
 
 for x in data:
     print(f"{x.text}\n\n")
